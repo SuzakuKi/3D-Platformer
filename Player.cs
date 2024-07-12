1a0f1b0b5d71bfdefb47dc3c2c8316eb785d4085
SuzakuKi <vladghoul@mail.ru>
2024-07-12 17:24:47 +0300

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    //Здоровье игрока
+    //Текущее здоровье игрока
     private int health = 10;
 
     //Число собранных монет
@@ -15,11 +16,29 @@ public class Player : MonoBehaviour
     public Transform attackPoint;
 
 
-    //Метод, понижающий здоровье игрока
+    //Компонент, отвечающий за проигрывание звуков
+    public AudioSource audioSource;
+
+    //Звуковой файл, содержащий звуковой эффект нанесения урона
+    public AudioClip damageSound;
+
+    //Метод, обрабатывающий нанесённый урон
     public void TakeDamage(int damage)
     {
         health -= damage;
-        print("Здоровье игрока: " + health);
+
+        //Если здоровье ещё есть, то проигрывается звук нанесения урона
+        if (health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+            //print("Здоровье игрока: " + health);
+        }
+        //Если здоровья нет, то перезапускается текущая сцена
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
     }
 
     //Метод, увеличивающий число монеток
@@ -35,7 +54,7 @@ public class Player : MonoBehaviour
 
         //Если игрок кликает левой кнопкой мыши, то создаётся огненный шар
         if (Input.GetMouseButtonDown(0))
-        {
+        {       
             Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
         }
 
